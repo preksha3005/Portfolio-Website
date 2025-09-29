63b4bd2c7e9f7fb00e2bf69bc1a2dfdb37267da2
preksha3005 <preksha.g504@gmail.com>
2025-09-29 18:34:18 +0530

    mail fix

--- server/index.js
+++ server/index.js
@@ -3,22 +3,21 @@ import mongoose from "mongoose";
 import cors from "cors";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 import Message from "./Models/Message.js";
+import Projects from "./Models/Projects.js";
 import cookieParser from "cookie-parser";
-import { Resend } from "resend";
 
 dotenv.config();
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://portfoliofrontend-6tgu.onrender.com",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: 'https://portfoliofrontend-6tgu.onrender.com',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use(express.json());
 app.use(cookieParser()); // middleware function in Express.js that enables the parsing of cookies in incoming requests.
@@ -34,24 +33,30 @@ mongoose
     process.exit(1);
   });
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
+// Send message from contact part of the portfolio
 app.post("/sendmessage", async (req, res) => {
   const { name, email, message } = req.body;
 
   if (!name || !email || !message) {
     return res.status(400).json({ error: "All fields are required" });
   }
-
   try {
-    await resend.emails.send({
-      from: process.env.EMAIL_REG,
-      to: process.env.EMAIL_USER,
-      subject: `Portfolio message from ${name}`,
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-      reply_to: email 
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.PASS,
+      },
     });
 
+    const mailOptions = {
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `Portfolio website message from ${name}`,
+      text: `You got a message!! \n\n Name: ${name} \n Email: ${email} \n Message: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
     const newMessage = new Message({
       name,
       email,
@@ -59,11 +64,9 @@ app.post("/sendmessage", async (req, res) => {
       createdAt: new Date(),
     });
     await newMessage.save();
-
     res.json({ message: "Message sent successfully!" });
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to send email" });
+    return res.status(500).json({ error: err.message });
   }
 });
 
